6533b9a1f33ce1c63c1c2c5c3d87606f3da8f691
Oleg mercenery <mercenery@inbox.ru>
2019-01-10 14:29:04 +0300

    reformated less than 80 chars long

--- CourseraLearn/exercise_2_penguins.py
+++ CourseraLearn/exercise_2_penguins.py
@@ -6,6 +6,7 @@ feet = '  ^^ ^^ '
 
 n = int(input())
 if n < 1000:
-    penguins = (hairP + '    ') * n + '\n' + (eyes + '   ') * n + '\n' + (beak + '  ') * n + '\n' + (
-                potbelly + ' ') * n + '\n' + (feet + '  ') * n + '\n'
+    penguins = (hairP + '    ') * n + '\n' + (eyes + '   ') * n + '\n' + (
+                beak + '  ') * n + '\n' + (
+                       potbelly + ' ') * n + '\n' + (feet + '  ') * n + '\n'
 print(penguins)
